ef2711094291702f0ae6c1c615a356265af461d9
Daniel Roy Greenfeld <pydanny@users.noreply.github.com>
2021-07-31 17:55:47 -0700

    Update [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -5,20 +5,20 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
 
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id)
   return {
     props: {
       postData
     }
-  }
-}
-
-export async function getStaticPaths() {
-  const paths = getAllPostIds()
-  return {
-    paths,
-    fallback: false
   }
 }
 
