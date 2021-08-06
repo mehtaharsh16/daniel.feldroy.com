b14440a0bea77cc892ef4e1349667842dff0825c
Daniel Roy Greenfeld <daniel.roy.greenfeld@octoenergy.com>
2021-08-06 08:45:03 -0700

    Update jobs

--- pages/jobs.js
+++ pages/jobs.js
@@ -11,15 +11,17 @@ export default function Jobs() {
       </Head>
       <section className={utilStyles.headingMd}>
         <h1 className={utilStyles.headingXl}>Jobs</h1>
-        <p>Jobs that I have vetted
+        <p>Employers and Jobs that I have vetted.
       </p>
 
         <ul>
           <li>
-            <a href="https://jobs.lever.co/octoenergy?location=Houston" target="_blank">Octopus Energy</a>
+            <a href="https://jobs.lever.co/octoenergy?location=Houston" target="_blank">Octopus Energy</a><br />
+            This is my company, providing renewable energy and fighting against climate change. How awesome is that? We do Python on the backend and React on the frontend.
           </li>
           <li>
-            <a href="" target="_blank">TODO</a>
+            <a href="https://sixfeetup.com/company/careers" target="_blank">Six Feet Up</a><br />
+            A consultancy run by friends of mine, Six Feet Up.
 
           </li>
         </ul>
