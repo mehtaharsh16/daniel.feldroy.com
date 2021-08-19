0f2f7e0901c8a6a0fb26f0c276d13c403ca0e60b
Daniel Roy Greenfeld <daniel.roy.greenfeld@octoenergy.com>
2021-08-18 21:41:35 -0700

    Attempt to push to master

---
+++ pages/stories/[id].js
@@ -0,0 +1,116 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/stories'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+import 'highlight.js/styles/gml.css';
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export default function Post({ postData }) {
+  const domain = "https://daniel.feldroy.com/"
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title} by {postData.author}</title>
+        <meta
+          name="description"
+          content={postData.description}
+        />
+        <meta property="og:title" content={postData.title} />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content={`https://daniel.feldroy.com/stories/${postData.id}`}
+        />
+
+        {postData.description ?
+          (
+            <meta
+              name="og:description"
+              content={postData.description}
+            />
+          ) : (
+            <meta
+              name="og:description"
+              content="Inside the Head of Daniel Roy Greenfeld (aka Daniel Feldroy)"
+            />
+          )
+        }
+
+        {/* Twitter card tags */}
+        <meta name="twitter:title" content={postData.title} />
+
+        {postData.image ?
+          (
+            <>
+              <meta
+                property="og:image"
+                content={`https://daniel.feldroy.com${postData.image}`}
+              />
+              <meta
+                name="twitter:image"
+                content={`https://daniel.feldroy.com${postData.image}`}
+              />
+            </>
+          ) : (
+            <>
+              <meta
+                property="og:image"
+                content="https://daniel.feldroy.com/images/profile.jpg"
+              />
+              <meta
+                name="twitter:image"
+                content="https://daniel.feldroy.com/images/profile.jpg"
+              />
+            </>
+          )
+        }
+
+        {/* Twitter card tags */}
+        <meta
+          name="twitter:description"
+          content={postData.description}
+        />
+
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div>
+          {postData.author}
+        </div>        
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        {postData.description &&
+          (
+            <div>
+              <small className={utilStyles.lightText}>
+                {postData.description}
+              </small>
+              <br />
+            </div>
+          )
+        }     
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
---
+++ pages/stories/index.js
@@ -0,0 +1,76 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout, { siteTitle } from '../../components/layout'
+import utilStyles from '../../styles/utils.module.css'
+import Date from '../../components/date'
+
+import { getSortedPostsData } from '../../lib/stories'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>Daniel Roy Greenfeld</title>
+        <meta
+          name="description"
+          content="Inside the head of Daniel Roy Greenfeld (aka Daniel Feldroy)"
+        />
+        <meta name="og:title" content="Daniel Roy Greenfeld" />
+        <meta
+          property="og:site_name"
+          content="Daniel Roy Greenfeld"
+        />
+
+
+        <meta
+          property="og:image"
+          content="https://daniel.feldroy.com/images/profile.jpg"
+        />
+        {/* Twitter card tags */}
+        <meta name="twitter:title" content="Daniel Feldroy" />
+        <meta
+          name="twitter:image"
+          content="https://daniel.feldroy.com/images/profile.jpg"
+        />
+      </Head>
+      <p>Our short stories are a mixture of humor, joy, fantasy, horror, and adventure, with a bit of space travel and fried chicken thrown in.</p>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title, description, author }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/stories/${id}`}>
+                <a>{title} </a> 
+              </Link>
+              <br />
+              <small>{author}</small>
+              <br />
+              {description &&
+                (
+                  <>
+                    <small className={utilStyles.lightText}>
+                      {description}
+                    </small>
+                    <br />
+                  </>
+                )
+              }
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
