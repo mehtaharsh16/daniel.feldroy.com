0bb92096533b1426fec72f9267e86f83fca62f38
Daniel Roy Greenfeld <daniel.roy.greenfeld@octoenergy.com>
2021-10-08 09:18:51 -0700

    Add drafts

--- pages/books/tech.js
+++ pages/books/tech.js
@@ -60,7 +60,7 @@ export default function TechBooks() {
             </Image>            
             <h2>Two Scoops of Django 3.x </h2>
             <p >
-              The 4th edition of Two Scoops of Django is out. With over 500 pages of material, you'll find best practices that will improve all your Django projects.
+              The 5th edition of Two Scoops of Django is out. With over 500 pages of material, you'll find best practices that will improve all your Django projects.
             </p>
             <p>
             <a href="https://transactions.sendowl.com/packages/772159/A329F48B/purchase" rel="nofollow"><img src="https://transactions.sendowl.com/assets/external/buy-now.png" /></a> 
@@ -76,7 +76,7 @@ export default function TechBooks() {
             </Image>            
             <h2>A Wedge of Django</h2>
             <p >
-              Our guided walkthrough tutorial where we build a real  production-quality Django web application from the ground up.
+              Our guided walkthrough tutorial where we build a real  production-quality Django 3.2 web application from the ground up.
             </p>
             <p>
             <a href="https://transactions.sendowl.com/packages/754641/749E0298/purchase" rel="nofollow"><img src="https://transactions.sendowl.com/assets/external/buy-now.png" /></a> 
