f25661536cf39b4a9381be73e32100b511a4270c
Daniel Roy Greenfeld <daniel.roy.greenfeld@octoenergy.com>
2022-01-30 11:49:53 -0800

    Add all the content from the old danielroygreenfeld.com site

--- legacy/legacy.py
+++ legacy/legacy.py
@@ -1,4 +1,10 @@
 """
+How to use this script:
+1. Go to your blogger account settings
+2. Search for the "Back up content" link
+3. Download the content as an XML file
+4. Run the script with python legacy.py <XML File> <DIRECTORY_OF_POSTS>
+
 TODOs
 1. Figure out if looking for 'Template: pydanny' can be automated
 2. Find pure python way to convert HTML to markdown
@@ -14,13 +20,18 @@ try:
 except ImportError:
     print("Install the dependencies in requirements.txt")
 
-def main(input_file: Path, output_dir: Path):
+
+def main(
+    input_file: Path,
+    output_dir: Path,
+    tag: str = typer.Option("legacy-blogger", help="Tag to add to frontmatter"),
+    show_original: bool = typer.Option(True, help="Link MD files to original articles"),
+):
 
     typer.secho(f"Parsing data from '{input_file}'", fg=typer.colors.GREEN)
     raw_text = input_file.read_text()
     # parse the historical data
     data = feedparser.parse(raw_text)
-    breakpoint()
     posts = {}
     for entry in data.entries:
         try:
@@ -46,18 +57,33 @@ def main(input_file: Path, output_dir: Path):
             continue
 
     # Write the markdown files
-    typer.secho(f"Writing {len(posts)} blogger posts to markdown files", fg=typer.colors.GREEN)
+    typer.secho(
+        f"Writing {len(posts)} blogger posts to markdown files", fg=typer.colors.GREEN
+    )
     with typer.progressbar(posts.items()) as posts_progress:
         for key, value in posts_progress:
             # Get a MD filename from the original HTML URL
             filename = key.replace(".html", ".md")
-            link = data['feed']['link'].replace('http', 'https')
+            filename = filename.replace(data["feed"]["link"], "")
+            link = data["feed"]["link"].replace("http", "https")
             filename = filename.replace(link, "")
+            # print('\n',link, data['feed']['link'], filename)
+            # Catches some of the configuration elements
+            if len(filename.strip()) == 0:
+                continue
+            # bypasses simple pages, TODO: Provide option to create MD pages
+            if filename.startswith("p-"):
+                continue
             filename = filename.replace("/", "-")
             # Get a list of tags
             tags = [x["term"] for x in value.tags]
-            tags = [x for x in tags if x != "http://schemas.google.com/blogger/2008/kind#post"]
-            tags.append("legacy-blogger")
+            tags = [
+                x
+                for x in tags
+                if x != "http://schemas.google.com/blogger/2008/kind#post"
+            ]
+            # Add the tag option to list of tags
+            tags.append(tag)
             frontmatter = {
                 "date": value["published"],
                 "published": True,
@@ -72,13 +98,16 @@ def main(input_file: Path, output_dir: Path):
                 f.write("---\n")
                 f.write(yaml.dump(frontmatter))
                 f.write("---\n\n")
-                # Set a link to the original content
-                f.write(f"*This was originally posted on blogger [here]({key})*.\n\n")
+                if show_original:
+                    # Set a link to the original content
+                    f.write(
+                        f"*This was originally posted on blogger [here]({key})*.\n\n"
+                    )
                 # Write the HTML, TODO: consider converting to markdown
                 f.write(value["summary"])
                 # If any comments, add them
                 if value["comments"]:
-                    f.write("\n\n---\n\n")            
+                    f.write("\n\n---\n\n")
                     f.write(
                         f'## {len(value["comments"])} comments captured from [original post]({key}) on Blogger\n\n'
                     )
@@ -90,5 +119,5 @@ def main(input_file: Path, output_dir: Path):
                         f.write("\n\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     typer.run(main)
