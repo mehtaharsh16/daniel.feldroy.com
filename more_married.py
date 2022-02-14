fb194d4327fcd59e2601aadbaea82cc17fe65fa8
Daniel Roy Greenfeld <daniel.roy.greenfeld@octoenergy.com>
2022-02-14 08:28:50 -0800

    Adding more_married script post

---
+++ more_married.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+import typer
+
+
+def main(met: datetime, celebration: datetime):
+    premarried = celebration - met
+
+    for i in range(2, 10):
+        duration = i * premarried
+        typer.secho(f"{i-1}x {(met + duration).date()}", fg=typer.colors.RED)
+
+
+if __name__ == "__main__":
+    typer.run(main)
