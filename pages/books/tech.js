32d1a8cc3d394ef79644efe162d537ef36258353
Daniel Roy Greenfeld <daniel.roy.greenfeld@octoenergy.com>
2022-03-11 15:09:44 -0800

    Add insutrctions on how to use pip and conda together

--- pages/books/tech.js
+++ pages/books/tech.js
@@ -1,21 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Script from "next/script"
-import Link from "next/link"
+import Head from "next/head";
+import Image from "next/image";
+import Script from "next/script";
+import Link from "next/link";
 
-import Layout from '../../components/layout'
-import utilStyles from '../../styles/utils.module.css'
-
+import Layout from "../../components/layout";
+import utilStyles from "../../styles/utils.module.css";
 
 export default function TechBooks() {
   return (
     <Layout>
       <Head>
         <title>Tech Books</title>
-        <meta
-          name="description"
-          content="Technical books I have co-written"
-        />
+        <meta name="description" content="Technical books I have co-written" />
         <meta property="og:title" content="Technical books I have co-written" />
         <meta property="og:type" content="website" />
         <meta
@@ -26,12 +22,15 @@ export default function TechBooks() {
         <meta
           name="og:description"
           content="With Audrey, I co-write dairy-themed technical books."
-        />  
+        />
         <meta
           name="twitter:description"
           content="With Audrey, I co-write dairy-themed technical books."
-        />        
-        <meta name="twitter:title" content="With Audrey, I co-write dairy-themed technical books." />  
+        />
+        <meta
+          name="twitter:title"
+          content="With Audrey, I co-write dairy-themed technical books."
+        />
         <meta
           property="og:image"
           content="https://daniel.feldroy.com/images/profile.jpg"
@@ -39,16 +38,14 @@ export default function TechBooks() {
         <meta
           name="twitter:image"
           content="https://daniel.feldroy.com/images/profile.jpg"
-        />            
-
-
+        />
       </Head>
       <article>
         <div className={utilStyles.backToHome}>
           <Link href="/books">
-              <a>← Back to books</a>
+            <a>← Back to books</a>
           </Link>
-        </div>          
+        </div>
         <h1 className={utilStyles.headingXl}>Tech Books I Have Co-Authored</h1>
         <p>With Audrey, I co-write dairy-themed technical books.</p>
         <ul className={utilStyles.list}>
@@ -56,15 +53,23 @@ export default function TechBooks() {
             <Image
               src="/images/Two-Scoops-of-Django-3-Alpha-Cover_1080x.jpg"
               height="514"
-              width="360">
-            </Image>            
+              width="360"
+            ></Image>
             <h2>Two Scoops of Django 3.x </h2>
-            <p >
-              The 5th edition of Two Scoops of Django is out. With over 500 pages of material, you'll find best practices that will improve all your Django projects.
+            <p>
+              The 5th edition of Two Scoops of Django is out. With over 500
+              pages of material, you'll find best practices that will improve
+              all your Django projects.
             </p>
             <p>
-            <a href="https://transactions.sendowl.com/packages/772159/A329F48B/purchase" rel="nofollow"><img src="https://transactions.sendowl.com/assets/external/buy-now.png" /></a> 
+              <a
+                href="https://transactions.sendowl.com/packages/772159/A329F48B/purchase"
+                rel="nofollow"
+              >
+                <img src="https://transactions.sendowl.com/assets/external/buy-now.png" />
+              </a>
             </p>
+
             <hr />
           </li>
 
@@ -72,21 +77,29 @@ export default function TechBooks() {
             <Image
               src="/images/AWOD-2021-06-29-8x10-Wedge-Front_1080x.png"
               height="450"
-              width="360">
-            </Image>            
+              width="360"
+            ></Image>
             <h2>A Wedge of Django</h2>
-            <p >
-              Our guided walkthrough tutorial where we build a real  production-quality Django 3.2 web application from the ground up.
+            <p>
+              Our guided walkthrough tutorial where we build a real
+              production-quality Django 3.2 web application from the ground up.
             </p>
             <p>
-            <a href="https://transactions.sendowl.com/packages/754641/749E0298/purchase" rel="nofollow"><img src="https://transactions.sendowl.com/assets/external/buy-now.png" /></a> 
+              <a
+                href="https://transactions.sendowl.com/packages/754641/749E0298/purchase"
+                rel="nofollow"
+              >
+                <img src="https://transactions.sendowl.com/assets/external/buy-now.png" />
+              </a>
             </p>
           </li>
-
         </ul>
-        <Script type="text/javascript" src="https://transactions.sendowl.com/assets/sendowl.js" strategy="afterInteractive"></Script>          
+        <Script
+          type="text/javascript"
+          src="https://transactions.sendowl.com/assets/sendowl.js"
+          strategy="afterInteractive"
+        ></Script>
       </article>
-    
     </Layout>
-  )
+  );
 }
