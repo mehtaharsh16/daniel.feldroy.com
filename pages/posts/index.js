304acbebd65dd47619193e77462ef8e98ce81680
Daniel Roy Greenfeld <daniel.roy.greenfeld@octoenergy.com>
2022-12-13 10:19:24 -0500

    Add search to articles index

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import utilStyles from "../../styles/utils.module.css";
 import MyDate from "../../components/date";
+import { useRouter } from "next/router";
+import MiniSearch from "minisearch";
 
 import { getSortedPostsData } from "../../lib/posts";
 
@@ -19,16 +21,42 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allPostsData, yearsBlogging }) {
+  // search setup
+  const router = useRouter();
+  const q = router.query.q || "";
+  let postsData = allPostsData;
+  if (q !== "") {
+    let miniSearch = new MiniSearch({
+      fields: ["id", "title", "description"], // fields to index for full-text search
+      storeFields: ["id", "title", "date", "description"], // fields to return with search results
+    });
+
+    miniSearch.addAll(allPostsData);
+    postsData = miniSearch.search(q);
+  }
   return (
     <>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h1>All Articles ({allPostsData.length})</h1>
-        <p>
-          Everything written by Daniel Roy Greenfeld for the past{" "}
-          {yearsBlogging} years
-        </p>
+        {q === "" && (
+          <>
+            <h1>All Articles ({allPostsData.length})</h1>
+            <p>
+              Everything written by Daniel Roy Greenfeld for the past{" "}
+              {yearsBlogging} years
+            </p>
+          </>
+        )}
+
+        {q !== "" && (
+          <>
+            <h1>
+              {postsData.length} articles on "{router.query.q}"
+            </h1>
+          </>
+        )}
+
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title, description }) => (
+          {postsData.map(({ id, date, title, description }) => (
             <li className={utilStyles.listItem} key={id}>
               {/* <h2 className={utilStyles.headingLg}>2022</h2> */}
               <Link href={`/posts/${id}`}>
