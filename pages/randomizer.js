e6e29cb5ac6ce05234fef9254b3db8983c35bf2b
Daniel Roy Greenfeld <daniel.roy.greenfeld@octoenergy.com>
2022-12-12 21:15:39 -0500

    Fixing the randomizer page

--- pages/randomizer.js
+++ pages/randomizer.js
@@ -1,33 +1,34 @@
-import Head from 'next/head'
-import utilStyles from '../styles/utils.module.css'
-import Layout from '../components/layout'
-import useSwr from 'swr'
+import Head from "next/head";
+import utilStyles from "../styles/utils.module.css";
+import Metatags from "../components/metatags";
+import useSwr from "swr";
 
-const fetcher = (url) => fetch(url).then((res) => res.json())
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 function Random() {
-  const { data, error } = useSwr('/api/random', fetcher)
+  const { data, error } = useSwr("/api/random", fetcher);
 
-  if (error) return <div>Oops!</div>
-  if (!data) return <div>Randomizing...</div>
-  return <div>{data.value}</div>  
+  if (error) return <div>Oops!</div>;
+  if (!data) return <div>Randomizing...</div>;
+  return <div>{data.value}</div>;
 }
 
 export default function Counter() {
-
+  const meta = {
+    title: "Randomizer",
+    description: "A demonstration of random and API calls",
+  };
   return (
-    <Layout>
-    <Head>
-      <title>Randomizer</title>
-    </Head>
-    <section className={utilStyles.headingMd}>
-    <h1 className={utilStyles.headingXl}>Randomizer</h1>
-    <p>Refresh the page to get a random number from the server.</p>
-    <h2><Random /></h2>
-
-    </section>
-    </Layout>
-  )
+    <>
+      <Metatags meta={meta} />
+      <section className={utilStyles.headingMd}>
+        <h1 className={utilStyles.headingXl}>Randomizer</h1>
+        <p>Refresh the page to get a random number from the server.</p>
+        <h2>
+          <Random />
+        </h2>
+      </section>
+    </>
+  );
 }
-
-
+``;
